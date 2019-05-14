3ca35d4ce58825bad38901ab8bfd521e72fe23e2
jontenthoff <jontenthoff@hotmail.com>
2019-05-14 15:57:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,18 +12,18 @@ class Student
     @grade = grade 
   end
   
-  def self.create_table
-    sql = CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade INTEGER)
-    DB[:conn].execute(sql)
-  end
+#   def self.create_table
+#     sql = CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade INTEGER)
+#     DB[:conn].execute(sql)
+#   end
   
-  def self.save
-    sql = INSERT INTO students (name, grade) VALUES (?, ?) 
-    DB[:conn].execute(sql, self.name, self.grade)
-  end
+#   def self.save
+#     sql = INSERT INTO students (name, grade) VALUES (?, ?) 
+#     DB[:conn].execute(sql, self.name, self.grade)
+#   end
   
-  def self.drop_table
-    sql = "DROP TABLE IF EXISTS students"
-    DB[:conn].execute(sql)
-  end
-end
+#   def self.drop_table
+#     sql = "DROP TABLE IF EXISTS students"
+#     DB[:conn].execute(sql)
+#   end
+# end
